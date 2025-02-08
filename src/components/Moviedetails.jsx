d76657c9af2358c988344b0bf91f40488fa2c770
deepak-05dktopG <kumardeepak59422@gmail.com>
2025-02-08 22:47:28 +0530

    set loader

--- src/components/Moviedetails.jsx
+++ src/components/Moviedetails.jsx
@@ -19,25 +19,34 @@ function Moviedetails({
 
 
     useEffect(() => {
-        const fetchShowDetails = async () => {
-            try {
-                const response = await axios.get(`https://api.tvmaze.com/shows/${id}`); // Fetching specific show details
-                setShowDetails(response.data);
-            } catch (error) {
-                console.error('Error fetching show details:', error);
-            }
-        };
 
-        fetchShowDetails();
+        setInterval(() => {
+
+            const fetchShowDetails = async () => {
+                try {
+                    const response = await axios.get(`https://api.tvmaze.com/shows/${id}`); // Fetching specific show details
+                    setShowDetails(response.data);
+                } catch (error) {
+                    console.error('Error fetching show details:', error);
+                }
+            };
+            fetchShowDetails();
+        }, 1500);
+
+
     }, [id]);
 
 
-    if (!showDetails) return <p className='Loading text-center align-content-center text-white' style={{ height: "100vh" }}> <svg class="pl" width="240" height="240" viewBox="0 0 240 240">
-        <circle class="pl__ring pl__ring--a" cx="120" cy="120" r="105" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 660" stroke-dashoffset="-330" stroke-linecap="round"></circle>
-        <circle class="pl__ring pl__ring--b" cx="120" cy="120" r="35" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 220" stroke-dashoffset="-110" stroke-linecap="round"></circle>
-        <circle class="pl__ring pl__ring--c" cx="85" cy="120" r="70" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 440" stroke-linecap="round"></circle>
-        <circle class="pl__ring pl__ring--d" cx="155" cy="120" r="70" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 440" stroke-linecap="round"></circle>
-    </svg></p>; // Loading state
+    if (!showDetails) return <p className='Loading text-center align-content-center' style={{ height: "100vh" }}>
+        <div class="loader">
+            <div class="loader__circle"></div>
+            <div class="loader__circle"></div>
+            <div class="loader__circle"></div>
+            <div class="loader__circle"></div>
+            <div class="loader__circle"></div>
+        </div>
+
+    </p>; // Loading state
 
     return (
         <>
